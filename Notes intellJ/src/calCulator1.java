97f9740d06e9a85fec15c519c9e60aebe2e5d527
SkylarGalloway <sgallowa@nebrwesleyan.edu>
2018-03-21 18:28:45 -0500

    sandwich

---
+++ Notes intellJ/src/calCulator1.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+public class calCulator1 {
+
+    private static double add(Scanner sysIn){
+        double first, second;
+        first= sysIn.nextDouble();
+        second = sysIn.nextDouble();
+
+        return first+second;
+    }
+    private static double subtract(Scanner sysIn){
+        double first, second;
+        first= sysIn.nextDouble();
+        second = sysIn.nextDouble();
+        return first-second;
+    }
+    private static double multiply(Scanner sysIn){
+        double first, second;
+        first= sysIn.nextDouble();
+        second = sysIn.nextDouble();
+        return first*second;
+    }
+    private static double divide(Scanner sysIn){
+        double first, second;
+        first= sysIn.nextDouble();
+        second = sysIn.nextDouble();
+        return first/second;
+    }
+    private static double mod(Scanner sysIn){
+        double first, second;
+        first= sysIn.nextDouble();
+        second = sysIn.nextDouble();
+        return first%second;
+    }
+    public static void main(String[] args){
+        System.out.println("This program will take a math operator (+,-,*,/,%) and then two numbers and return the result");
+        Scanner sysIn = new Scanner(System.in);
+        char opp=sysIn.next().charAt(0);
+
+        double result =0.0;
+
+        switch(opp){
+            case '+':
+                result = add(sysIn);
+                break;
+            case '-':
+                result = subtract(sysIn);
+                break;
+            case '*':
+                result = multiply(sysIn);
+                break;
+            case '/':
+                result = divide(sysIn);
+                break;
+            case '%':
+                result = mod(sysIn);
+                break;
+        }
+        System.out.println(result);
+
+
+
+    }
+}
+
